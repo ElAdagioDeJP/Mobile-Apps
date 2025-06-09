e7d3f454f65ec9819eee9b8ebc371c7299dc609d
ElAdagioDeJP <129967733+ElAdagioDeJP@users.noreply.github.com>
2025-06-09 11:12:46 -0400

    Yo creo que si se pudo, ahora si, backend funciona, troll

--- Proyecto/fe-score/config.js
+++ Proyecto/fe-score/config.js
@@ -1,11 +1,11 @@
 // FE-SCORE/config.js
 import { Platform } from "react-native";
-// Cuando estés en desarrollo
-const DEV_API = Platform.OS === "android"
-  ? "http://10.0.2.2:5000"   // Android emulator
-  : "http://localhost:5000"; // iOS simulator o expo web
 
-// En producción (URL de tu deploy en Heroku/Railway/etc.)
-const PROD_API = "mobile-apps-production.up.railway.app";
+const DEV_API = Platform.OS === "android"
+  ? "http://10.0.2.2:5000"
+  : "http://localhost:5000";
+
+// En producción (URL de tu deploy en Railway)
+const PROD_API = "https://mobile-apps-production.up.railway.app"; // <--- PEGA LA URL AQUÍ
 
 export const API_URL = __DEV__ ? DEV_API : PROD_API;
