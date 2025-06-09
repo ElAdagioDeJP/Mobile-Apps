644344585de630d1018e96c9c8059ee4feddeb48
ElAdagioDeJP <129967733+ElAdagioDeJP@users.noreply.github.com>
2025-06-09 14:26:36 -0400

    au

--- Proyecto/backend/hashedpasswords.py
+++ Proyecto/backend/hashedpasswords.py
@@ -1,4 +1,4 @@
 from werkzeug.security import generate_password_hash
-password = ""  # Usa la contraseña que deseas
+password = "entrenador1234"  # Usa la contraseña que deseas
 hashed_password = generate_password_hash(password)
 print(hashed_password)
